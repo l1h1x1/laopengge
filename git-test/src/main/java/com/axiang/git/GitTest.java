c4f8cd473c6148d928171026da55d35c477ef566
axiang <axiang@guigu.com>
2023-09-24 15:08:44 +0800

    first

---
+++ git-test/src/main/java/com/axiang/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.axiang.git;
+
+/**
+ * @author AXiang
+ * @create 2023/9/24 15:07
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("world");
+        System.out.println("hello");
+        System.out.println("world");
+    }
+}
